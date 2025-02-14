bb4b647021f1f91a3482d756338fa86362c4ef74
James <jamesbmadden@gmail.com>
2025-02-14 17:59:34 -0500

    write out algorithm

--- board/board.ino
+++ board/board.ino
@@ -2,6 +2,9 @@
  * the main setup and loop running on the board
  */
 
+// set the colour that was last handled so we don't duplicate actions on blue or green
+String handled_colour = "white";
+
 void setup() {
 
   // set up the colour classifier
@@ -36,6 +39,40 @@ void setup() {
 }
 
 void loop() {
+
+  // STEP 1: course correction
+  IRReading ir = read_ir();
+
+  // check if rotation is needed
+  if (ir.left && !ir.right) {
+    // turn slightly left
+    rotate_left(100, 200);
+  } else if (ir.right && !ir.left) {
+    // turn slightly right
+    rotate_right(100, 200);
+  }
+
+  // STEP 2: colour action
+  // take a colour reading
+  Colour reading = read_colour();
+  String colour = classify_colour(reading.red, reading.green, reading.blue);
+
+  // take action based on the colour
+  if (colour.compareTo("blue") == 0 && handled_colour.compareTo("blue") != 0) {
+    // lake! pause here
+    delay(7000);
+    // then keep going
+    go_forward(200, 200);
+  } else if (colour.compareTo("green") == 0 && handled_colour.compareTo("green") != 0) {
+    // activate wiggles to drop seeds
+    wiggle(50);
+  }
+
+  // move forwards and set handled colour
+  handled_colour = colour;
+  go_forward(200, 200);
+
+
 
   // motors forward
   // check black - if either are colour/white, adjust accordingly
