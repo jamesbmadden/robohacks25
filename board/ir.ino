b974654d6664656de94705595ddb1b8cae78db72
James <jamesbmadden@gmail.com>
2025-02-14 16:27:50 -0500

    create file for ir reading

---
+++ board/ir.ino
@@ -0,0 +1,38 @@
+/**
+* utility functions for using the infrared sensor
+*/
+#define IR_LIN 2
+#define IR_RIN 1
+
+// 
+typedef struct IRReading {
+  int left;
+  int right;
+};
+
+/**
+ * read from the sensor and return 1 for black, 0 for lighter colour.
+ * in order for this to work, the sensor has to be calibrated properly. use the calibration script under the ir_calibration folder for that
+ */
+int is_black (int pin) {
+
+  // value is HIGH when the measure is black
+  int val = digitalRead(pin);
+
+  return val == HIGH;
+
+}
+
+/**
+ * read from both sensors and return both results
+ */
+IRReading read_ir () {
+
+  IRReading results;
+
+  results.left = is_black(IR_LIN);
+  results.right = is_black(IR_RIN);
+
+  return results;
+
+}
