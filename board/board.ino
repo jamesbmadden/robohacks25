8cde6c924e845c6582719f32a263d7471f3d9ef8
James <jamesbmadden@gmail.com>
2025-02-14 17:50:00 -0500

    describe algorithm and activate all pins

--- board/board.ino
+++ board/board.ino
@@ -1,3 +1,7 @@
+/**
+ * the main setup and loop running on the board
+ */
+
 void setup() {
 
   // set up the colour classifier
@@ -8,10 +12,20 @@ void setup() {
   pinMode(C_S1, OUTPUT);
   pinMode(C_S2, OUTPUT);
   pinMode(C_S3, OUTPUT);
-  
-  // set the colour sensor output
-  pinMode(C_OUT, INPUT);
+  pinMode(C_IN, INPUT);
   pinMode(IR_IN, INPUT);
+
+  // start the IR pins
+  pinMode(IR_LIN, INPUT);
+  pinMode(IR_RIN, INPUT);
+
+  // start the motor pins
+  pinMode(M_SA, OUTPUT);
+  pinMode(M_SB, OUTPUT);
+  pinMode(M_1, OUTPUT); // right reverse
+  pinMode(M_2, OUTPUT); // right forward
+  pinMode(M_3, OUTPUT); // left reverse
+  pinMode(M_4, OUTPUT); // left forward
   
   // set frequency scaling to 20%
   digitalWrite(C_S0, HIGH);
@@ -20,9 +34,16 @@ void setup() {
   // turn on serial communication
   Serial.begin(9600);
 }
+
 void loop() {
+
+  // motors forward
+  // check black - if either are colour/white, adjust accordingly
+  // read colour - if green drop seed by wiggling/vibrating
+  //               if blue wait
+  //               if red/white keep going
   
-  // read a colour
+  /*// read a colour
   Colour reading = read_colour();
 
   // output the colour
@@ -35,7 +56,11 @@ void loop() {
   Serial.println(reading.blue);
 
   // check what the reading is
-  String result = classify_colour(red, green, blue);
+  String result = classify_colour(reading.red, reading.green, reading.blue);
   Serial.print("detected colour was ");
   Serial.println(result);
+
+  // wait a second
+  delay(1000);*/
+
 }
