c55125584aec98b26410d41b5fb6b7108c17c75d
James <jamesbmadden@gmail.com>
2025-02-14 20:36:30 -0500

    refactor + initial algorithm design finalized

--- board/board.ino
+++ board/board.ino
@@ -1,11 +1,14 @@
 /**
  * the main setup and loop running on the board
  */
+#include "colour.h"
+#include "ir.h"
+#include "motor.h"
 
 // set the colour that was last handled so we don't duplicate actions on blue or green
 String handled_colour = "white";
 
-void setup() {
+void setup () {
 
   // set up the colour classifier
   set_colours();
@@ -16,7 +19,6 @@ void setup() {
   pinMode(C_S2, OUTPUT);
   pinMode(C_S3, OUTPUT);
   pinMode(C_IN, INPUT);
-  pinMode(IR_IN, INPUT);
 
   // start the IR pins
   pinMode(IR_LIN, INPUT);
@@ -28,7 +30,7 @@ void setup() {
   pinMode(M_1, OUTPUT); // right reverse
   pinMode(M_2, OUTPUT); // right forward
   pinMode(M_3, OUTPUT); // left reverse
-  pinMode(M_4, OUTPUT); // left forward
+  pinMode(M_4, OUTPUT); // left forward*
   
   // set frequency scaling to 20%
   digitalWrite(C_S0, HIGH);
@@ -38,18 +40,17 @@ void setup() {
   Serial.begin(9600);
 }
 
-void loop() {
-
+void loop () {
   // STEP 1: course correction
   IRReading ir = read_ir();
 
   // check if rotation is needed
   if (ir.left && !ir.right) {
     // turn slightly left
-    rotate_left(100, 200);
+    rotate_left(100, 0.75);
   } else if (ir.right && !ir.left) {
     // turn slightly right
-    rotate_right(100, 200);
+    rotate_right(100, 0.75);
   }
 
   // STEP 2: colour action
@@ -60,44 +61,18 @@ void loop() {
   // take action based on the colour
   if (colour.compareTo("blue") == 0 && handled_colour.compareTo("blue") != 0) {
     // lake! pause here
+    move_to_centre();
     delay(7000);
     // then keep going
-    go_forward(200, 200);
+    go_forward(200, 0.75);
   } else if (colour.compareTo("green") == 0 && handled_colour.compareTo("green") != 0) {
+    move_to_centre();
     // activate wiggles to drop seeds
     wiggle(50);
   }
 
   // move forwards and set handled colour
   handled_colour = colour;
-  go_forward(200, 200);
-
-
-
-  // motors forward
-  // check black - if either are colour/white, adjust accordingly
-  // read colour - if green drop seed by wiggling/vibrating
-  //               if blue wait
-  //               if red/white keep going
-  
-  /*// read a colour
-  Colour reading = read_colour();
-
-  // output the colour
-  Serial.println("== READING ==");
-  Serial.print("Red: ");
-  Serial.print(reading.red);
-  Serial.print(" Green: ");
-  Serial.print(reading.green);
-  Serial.print(" Blue: ");
-  Serial.println(reading.blue);
-
-  // check what the reading is
-  String result = classify_colour(reading.red, reading.green, reading.blue);
-  Serial.print("detected colour was ");
-  Serial.println(result);
-
-  // wait a second
-  delay(1000);*/
+  go_forward(200, 0.75);
 
 }
