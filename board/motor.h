4dd653ed3977de0d56255a5090d8b53264abeaf4
James <jamesbmadden@gmail.com>
2025-02-14 20:34:42 -0500

    fix motor header

--- board/motor.h
+++ board/motor.h
@@ -1,7 +1,7 @@
 // motor pins
-#define M_SA = 14
-#define M_SB = 15
-#define M_1 = 16
-#define M_2 = 17
-#define M_3 = 18
-#define M_4 = 19
+#define M_SA 14
+#define M_SB 15
+#define M_1 16
+#define M_2 17
+#define M_3 18
+#define M_4 19
